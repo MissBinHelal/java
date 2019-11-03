1105e6083029d3c9a5c537803901a8632ba2a71c
MissBinHelal <amalhelal.cs@gmail.com>
2019-11-03 21:58:14 +0300

    add new assigment zoo keeper

---
+++ ZooKeeper/src/Gorilla.java
@@ -0,0 +1,19 @@
+
+public class Gorilla extends Mammal {
+public void throwSomething() {
+	System.out.println("thrown soemthing");
+	energyLevel-=5;
+}
+public void eatBananas() {
+	System.out.println("good");
+	energyLevel+=10;
+
+
+}
+public void climb() {
+	System.out.println("climbed a tree");
+	energyLevel-=10;
+
+
+}
+}
